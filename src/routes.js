7f0b6be1404ab87ac52232229cfc5ad79108a0b1
matin0303 <pirmohammadimatin@gmail.com>
2024-04-07 04:48:55 +0330

    create routes

---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import Home from "./pages/home/Home"
+import UsersList from "./pages/users/UserList"
+import NewUser from "./pages/newUser/NewUser"
+import Products from "./pages/products/Products"
+
+
+let routes = [
+    {path : '/' , element:<Home/>},
+    {path : '/users' , element:<UsersList/>},
+    {path : '/newUser' , element:<NewUser/>},
+    {path : '/products', element :<Products/>}
+]
+
+export default routes
