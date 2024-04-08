412d232c79995e0af736dac2364ec9a8cae4cfd7
matin0303 <pirmohammadimatin@gmail.com>
2024-04-08 04:10:02 +0330

    create chart in Home page

---
+++ src/datas.js
@@ -0,0 +1,52 @@
+let xAxisData = [
+    {
+        'name' : 'Jan',
+        'sale' : 112_000
+    },
+    {
+        'name' : 'Feb',
+        'sale' : 83_000
+    },
+    {
+        'name' : 'Mar',
+        'sale' : 74_200
+    },
+    {
+        'name' : 'Apr',
+        'sale' : 35_000
+    },
+    {
+        'name' : 'May',
+        'sale' : 96_000
+    },
+    {
+        'name' : 'Jun',
+        'sale' : 85_000
+    },
+    {
+        'name' : 'Jul',
+        'sale' : 60_000
+    },
+    {
+        'name' : 'Agu',
+        'sale' : 93_000
+    },
+    {
+        'name' : 'Sep',
+        'sale' : 89_000
+    },
+    {
+        'name' : 'Oct',
+        'sale' : 73_000
+    },
+    {
+        'name' : 'Nov',
+        'sale' : 159_700
+    },
+    {
+        'name' : 'Dev',
+        'sale' : 89_000
+    },
+
+]
+export {xAxisData}
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-import Features from '../../components/features/Features'
+import React from 'react';
+import Features from '../../components/features/Features';
+import './Home.css';
+import Chart from '../../components/chart/Chart'
+import { xAxisData } from '../../datas';
 
 export default function Home() {
   return (
+    <div className='home'>
       <Features/>
+      <Chart grid title='Month Sale' data={xAxisData} dataKey='sale'/>
+    </div> 
   )
 }
